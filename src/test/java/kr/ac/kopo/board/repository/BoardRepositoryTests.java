a5fe780858f5b75be2de68856dcb651b47c5453f
realCCC <wpdls2821@gmail.com>
2023-09-01 16:17:03 +0900

    테스트 작성

--- src/test/java/kr/ac/kopo/board/repository/BoardRepositoryTests.java
+++ src/test/java/kr/ac/kopo/board/repository/BoardRepositoryTests.java
@@ -1,11 +1,19 @@
 package kr.ac.kopo.board.repository;
 
+import jakarta.transaction.Transactional;
 import kr.ac.kopo.board.entity.Board;
 import kr.ac.kopo.board.entity.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -14,8 +22,8 @@ public class BoardRepositoryTests {
     private BoardRepository boardRepository;
 
     @Test
-    public void insertBoard(){
-        IntStream.rangeClosed(1, 100).forEach(i->{
+    public void insertBoard() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
 
             Member member = Member.builder()
                     .email("user" + i + "@kopo.ac.kr")
@@ -27,6 +35,42 @@ public class BoardRepositoryTests {
                     .writer(member)//FK가 설정된 컬럼 값을 삽입할 때는 참조하는 Member Entity의 객체 참조값을 사용
                     .build();
             boardRepository.save(board);
+        });
+    }
+
+    @Transactional
+    @Test
+    public void testRead1() {
+        Optional<Board> result = boardRepository.findById(100L); //100L은 검색할 글번호를 뜻함
+
+        Board board = result.get();
+        System.out.println(board);
+        System.out.println(board.getWriter());
+    }
+
+    @Test
+    public void testReadWithWriter() {
+        Object result = boardRepository.getBoardWithWriter(50L);
+        Object[] arr = (Object[]) result;
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void testGetBoardWithReply() {
+        List<Object[]> result = boardRepository.getBoardWithReply(5L);
+
+        for (Object[] arr : result) {
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    @Test
+    public void testWithReplyCount() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+        result.get().forEach(row -> {
+            Object[] arr = (Object[]) row;
+            System.out.println(Arrays.toString(arr));
         });
     }
 }
