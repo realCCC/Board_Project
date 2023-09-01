d96d8a80ea1096318570120f6a80d3788bc9a09f
realCCC <wpdls2821@gmail.com>
2023-09-01 16:17:03 +0900

    목록화면에 사용될 결과행을 출력하는 테스트

--- src/main/java/kr/ac/kopo/board/entity/Board.java
+++ src/main/java/kr/ac/kopo/board/entity/Board.java
@@ -16,10 +16,12 @@ public class Board extends BaseEntity{
     private Long bno;
 
     private String title;
-
     private String content;
 
-    @ManyToOne
+//  지연(Lazy) 로딩 방식 설정
+//  즉각(Eager) 로딩 방식은 기본설정되어있음, 모든 관계에 대해 Join을 하기 때문에 불필요한 조인이 발생함
+//    데이터의 양이 많을수록 즉각로딩 방식이 실행시간오래걸림
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member writer;//board 테이블에 writer_email 컬럼생성이 되고 FK(Member 테이블의 email컬럼 값만 참조)가 설정됨
 
 }
--- src/main/java/kr/ac/kopo/board/repository/BoardRepository.java
+++ src/main/java/kr/ac/kopo/board/repository/BoardRepository.java
@@ -1,7 +1,28 @@
 package kr.ac.kopo.board.repository;
 
 import kr.ac.kopo.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    //  Lazy방식에서는 Join이 필요할 때 JPQL을 사용해서 Join에 필요한 sql문을 작성해야함.
+    @Query("select b, w from Board b left join b.writer w where b.bno =:bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
+
+    //  목록화면에 사용될 데이터행의 결과 집합을 얻을 수 있는 JPQL
+    @Query("select b, r from Board b left join Reply r ON r.board = b where b.bno =:bno")
+    List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    //  목록화면에 사용될 데이터행의 결과 + 댓글의 갯수 : JPQL
+    @Query(value = "select b, w, count(r) from Board b" +
+            " left join b.writer w" +
+            " left join Reply r ON r.board = b " +
+            "group by b",
+            countQuery = "select count(b) from Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable);
 }
