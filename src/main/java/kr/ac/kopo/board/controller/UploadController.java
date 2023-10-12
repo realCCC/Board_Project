201c7f6fcc8a6d1ddc589b32b47b9833e6d69dca
realCCC <wpdls2821@gmail.com>
2023-10-12 15:44:44 +0900

    Merge remote-tracking branch 'origin/master'

--- src/main/java/kr/ac/kopo/board/controller/UploadController.java
+++ src/main/java/kr/ac/kopo/board/controller/UploadController.java
@@ -1,18 +1,28 @@
 package kr.ac.kopo.board.controller;
 
-
+import kr.ac.kopo.board.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -23,34 +33,38 @@ public class UploadController {
     @Value("${kr.ac.kopo.upload.path}") // application.properties의 변수
     private String uploadPath;
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles) {
-        for (MultipartFile uploadFile : uploadFiles) {
-            if (!uploadFile.getContentType().startsWith("image")) { // 이미지 파일만 업로드 가능
-                log.warn("This file is not an image type");
-                return;
-            }
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) {
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
+        for (MultipartFile uploadFile : uploadFiles) {
+            // 이미지 파일만 업로드 가능
+            if (uploadFile.getContentType().startsWith("image") == false) {
+                log.warn("이미지 파일형식이 아닙니다.");
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
             // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
             String originalName = uploadFile.getOriginalFilename();
             String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
-            log.info("fileName: " + fileName);
+            log.info("파일명: " + fileName);
 
-            // 날짜 폴더 생성
+            // 날짜 폴더 생성1
             String folderPath = makeFolder();
 
+            // 업로드 파일의 이름이 중복되서 기존의 파일이 덮어쓰기 되는 것을 방지하기 위해 랜덤한 uuid 값을 파일명 앞에 추가
             // UUID 생성
             String uuid = UUID.randomUUID().toString();
 
             // 저장할 파일 이름 중간에 "_"를 이용해서 구분
             String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
             Path savePath = Paths.get(saveName);
-
             try {
                 uploadFile.transferTo(savePath);
+                resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
 
             }
-        } // end for
+        }
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder() {
@@ -66,6 +80,26 @@ public class UploadController {
         return folderPath;
     }
 
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName){
+        ResponseEntity<byte[]> result = null;
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTf-8");
+            log.info("fileName:" + srcFileName);
 
+            File file = new File((uploadPath + File.separator + srcFileName));
+            log.info("file: " + file);
 
+            HttpHeaders headers = new HttpHeaders();
+
+            headers.add("Content-Type", Files.probeContentType(file.toPath()));
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), headers, HttpStatus.OK);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
--- src/main/java/kr/ac/kopo/board/dto/UploadResultDTO.java
+++ src/main/java/kr/ac/kopo/board/dto/UploadResultDTO.java
@@ -1,9 +1,14 @@
 package kr.ac.kopo.board.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+@Data
+@AllArgsConstructor
 public class UploadResultDTO implements Serializable {
     private String fileName;
     private String uuid;
